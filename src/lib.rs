3881f1794606389176f7c2d630b9d354e5f5d45f
Michael Coulter <mjc@sekurity.org>
2022-04-14 22:39:42 +0000

    add README add tracing/color-eyre skeleton move to lib.rs indirection due to personal preferences as to how it works with testing

---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+use color_eyre::eyre;
+
+pub fn start() -> eyre::Result<()> {
+   println!("Hello crates.io!"); 
+   Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, crates.io!");
+use tracing::trace;
+use color_eyre::eyre;
+
+fn main() -> eyre::Result<()> {
+    install_tracing();
+    trace!("tracing installed.");
+    color_eyre::install()?;
+    trace!("color_eyre installed.");
+
+    blackbag::start()
+}
+
+fn install_tracing() {
+use tracing_subscriber::prelude::*;
+use tracing_subscriber::fmt::{format,layer,time};
+use tracing_subscriber::EnvFilter;
+use tracing_error::ErrorLayer;
+
+    let fmt_layer = layer()
+        .with_target(false)
+        .with_span_events(format::FmtSpan::ACTIVE)
+        .with_writer(std::io::stderr)
+        .with_timer(time::LocalTime::rfc_3339());
+    let filter_layer = EnvFilter::try_from_env("BKB_LOG")
+        .or_else(|_| EnvFilter::try_new("info"))
+        .unwrap();
+
+    tracing_subscriber::registry()
+        .with(filter_layer)
+        .with(fmt_layer)
+        .with(ErrorLayer::default())
+        .init();
 }
